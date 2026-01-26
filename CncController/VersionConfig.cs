cbc8a9ba3820c91920a87cab15711060c5904fa3
segaqoo-eng <segaqoo@gmail.com>
2026-01-26 18:37:22 +0800

    更新版本號與隨機字串於 VersionConfig.cs
    更新 VersionConfig.cs：將 CurrentVersion 改為 "2026.01.26_SYNC_CHECK_05"，RadomStr 改為 "asdf"。

--- CncController/VersionConfig.cs
+++ CncController/VersionConfig.cs
@@ -3,7 +3,7 @@
     public static class VersionConfig
     {
         // 每次修改程式時，更新這個字串 
-        public const string CurrentVersion = "2026.01.26_SYNC_CHECK_04";
-        public const string RadomStr = "qwer";
+        public const string CurrentVersion = "2026.01.26_SYNC_CHECK_05";
+        public const string RadomStr = "asdf";
     }
 }

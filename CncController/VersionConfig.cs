672c057581f3c174744ea5f8ebce178294b6c1d2
segaqoo-eng <segaqoo@gmail.com>
2026-01-28 11:03:13 +0800

    #2026.01.28_SYNC_CHECK_01

--- CncController/VersionConfig.cs
+++ CncController/VersionConfig.cs
@@ -3,7 +3,7 @@
     public static class VersionConfig
     {
         // 每次修改程式時，更新這個字串 
-        public const string CurrentVersion = "2026.01.26_SYNC_CHECK_05";
-        public const string RadomStr = "asdf";
+        public const string CurrentVersion = "2026.01.28_SYNC_CHECK_01";
+        public const string RadomStr = "qaz";
     }
 }
